457786590354d7b09ac0b56d597277a7539cc756
kcaputo <kelly.caputo@journalism.cuny.edu>
2016-02-17 21:55:36 -0500

    Update metro.js

--- metro.js
+++ metro.js
@@ -21,20 +21,3 @@ function calcAlert(){
 		}
 	
 }
-
-
-
-
-/*var rent;
-var income;
-var percentage;
-function rentCalc(){
-	rent = document.getElementById("rent").value;
-	income = document.getElementById("income").value;
-	percentage = rent / income * 100;
-	alert (percentage + 
-		" percent of your $" + 
-		income  + 
-		" in income is spent on rent!");
-	//alert("The rent is $" + rent + " and your income is $" + income);
-}*/
